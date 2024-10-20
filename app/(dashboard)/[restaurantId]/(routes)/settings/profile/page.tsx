41e6784962ec4106aecfaf2063401d7f26be34c2
kaustabhws <kaustabhworkspace@gmail.com>
2024-10-20 15:18:38 +0530

    Refactor navigation links to include profile in settings

--- app/(dashboard)/[restaurantId]/(routes)/settings/profile/page.tsx
+++ app/(dashboard)/[restaurantId]/(routes)/settings/profile/page.tsx
@@ -1,7 +1,6 @@
 import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-import { ProfileForm } from "../components/profile-form"; 
 import { SettingsForm } from "../components/settings-form";
 
 interface ProfilePageProps {
