f2522b6da5dd7c8375f34590330a4502f3ab2284
Kaustabh Mahanta <kaustabhworkspace@gmail.com>
2025-02-28 16:10:34 +0530

    Enhance authentication layout with responsive design; add background image for smaller screens

--- app/(auth)/(routes)/layout.tsx
+++ app/(auth)/(routes)/layout.tsx
@@ -1,10 +1,27 @@
+import Image from "next/image";
+
 export default function AuthLayout({
-    children,
-  }: {
-    children: React.ReactNode;
-  }) {
-    return (
-      <div className="flex items-center justify-center min-h-screen">{children}</div>
-    );
-  }
-  
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex min-h-screen w-full max-[830px]:flex-col max-[830px]:items-center max-[830px]:justify-center max-[830px]:bg-cover max-[830px]:bg-center auth-bg">
+      {/* Left Side - Image (Hidden on Small Screens) */}
+      <div className="w-1/2 h-screen relative max-[830px]:hidden">
+        <Image 
+          src="/login-hero.png" 
+          alt="Login Hero"
+          layout="fill"
+          objectFit="contain"
+          unoptimized
+        />
+      </div>
+      
+      {/* Right Side - Children */}
+      <div className="w-1/2 flex items-center justify-center p-10 max-[830px]:w-full max-[830px]:p-5">
+        {children}
+      </div>
+    </div>
+  );
+}
